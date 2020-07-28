4a36b4ebd5817f3401231e987ca0efa95fa58987
Daniel Perz <dpere316@fiu.edu>
2020-07-28 14:05:38 -0400

    styles

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,4 +4,19 @@ router.get('/', (req, res, next) => {
   res.status(200).json({ msg: 'Working' });
 });
 
+router.get('/home',(req,res,next) => {
+ let styles = {
+    header: {
+      backgroundColor: "blue",
+    },
+    ul: {
+      backgroundColor: "green",
+    },
+  }
+
+  res.status(200).json({ styles });
+
+
+});
+
 module.exports = router;
