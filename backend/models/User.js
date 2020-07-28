93df24e6188eb87dd7acdc4df3a2c9c24aa51825
Daniel Perz <dpere316@fiu.edu>
2020-07-28 14:52:16 -0400

    signup

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,9 +4,17 @@ const PLM = require("passport-local-mongoose");
 const userSchema = new Schema(
   {
     email: String,
-    name: String,
+    name: 
+    { 
+      firstname:String,
+      lastname:String,
+    },
     googleId: String,
     imageUrl: String,
+    song:String,
+    friends:[String],
+    bio:String,
+
   },
   {
     timestamps: true,
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,10 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
+const Users = require('../models/User')
 
 router.get('/', (req, res, next) => {
   res.status(200).json({ msg: 'Working' });
 });
+
 
 module.exports = router;
