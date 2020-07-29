04007b209186f189ea4b7e30c7258e38a05cf0dd
Daniel Perz <dpere316@fiu.edu>
2020-07-29 17:44:05 -0400

    Add and remove friends works perfectly

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,32 +6,63 @@ router.get("/", (req, res, next) => {
   res.status(200).json({ msg: "Working" });
 });
 
+// Set status at profile page
 router.post("/profile/status", isAuth, (req, res, next) => {
   User.findByIdAndUpdate(req.user._id, req.body, { new: true }).then((user) => {
     res.json({ user });
   });
 });
 
+// Displays the status of all friends on feed
+router.get("/get-status", isAuth, (req, res, next) => {
+  User.find().then((status) => {
+    console.log("user",status)
+    res.json({ status });
+  });
+})
+
+//Displays a list of users 
 router.get("/find-friends", isAuth, (req, res, next) => {
-  User.find().then((users) => {
+  User.find({_id:{$ne:req.user._id}}).then((users) => {
+    console.log("user",users)
     res.json({ users });
   });
 });
 
+//Add Friends
 router.post("/add-friends", isAuth, (req, res, next) => {
   console.log(
     "The person who clicked the button",
     req.user._id,
     "Befriended",
     req.body._id
-  );
+  )
   User.findByIdAndUpdate(req.user._id, {
-    $push: { friends: req.body._id },
+    $addToSet: { friends: req.body._id },
   }).then((users) => {
     User.findByIdAndUpdate(req.body._id, {
-      $push: { friends: req.user._id },
+      $addToSet: { friends: req.user._id },
     }).then((users) => {
       res.json({ friends: true });
+    });
+  });
+});
+
+// Remove friend from list
+router.post("/remove-friends", isAuth, (req, res, next) => {
+  console.log(
+    "The person who clicked the button",
+    req.user._id,
+    "Removed Friend",
+    req.body._id
+  );
+  User.findByIdAndUpdate(req.user._id, {
+    $pull: { friends: req.body._id },
+  }).then((users) => {
+    User.findByIdAndUpdate(req.body._id, {
+      $pull: { friends: req.user._id },
+    }).then((users) => {
+      res.json({ friends: false });
     });
   });
 });
@@ -44,8 +75,7 @@ router.post("/add-friends", isAuth, (req, res, next) => {
 
 
 
-
-
+// Is logged in
 function isAuth(req, res, next) {
   req.isAuthenticated()
     ? next()
