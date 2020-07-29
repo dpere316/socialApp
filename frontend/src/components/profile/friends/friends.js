b51cc77b211250f21f51cff572f1b0ae849cf758
Daniel Perz <dpere316@fiu.edu>
2020-07-29 11:29:52 -0400

    nothing

---
+++ frontend/src/components/profile/friends/friends.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import actions from '../../../services'
+
+class friends extends Component {
+    state={
+        users:[]
+    }
+    async componentDidMount() {
+    let res = await actions.findFriends(this.state)
+    console.log(res)
+    this.setState({
+        users:res.data.users
+    })  
+    }
+
+    displayUsers = () =>{
+        return this.state.users.map((eachUser) => {
+            console.log(eachUser)
+            return <div>
+                <li>
+                    {eachUser.firstname}
+                    <button onClick={() => this.addFriend(eachUser)}>Add Friend</button>
+                    <button onClick={()=> this.removeFriend(eachUser)}>Remove Friend</button>
+                </li>
+            </div>
+        })
+    }
+
+    addFriend = async (friend) =>
+    {
+        let res = await actions.addFriend(friend)
+        console.log(res)
+    }
+    render() {
+        return (
+            <div>
+                {this.displayUsers()}
+            </div>
+        );
+    }
+}
+
+export default friends;
