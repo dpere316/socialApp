8a4c44c4ed5658f2742ef64b16a7eb7474be7c19
rltrtl <rickytoledo@LAPTOP-3UVQIICU.localdomain>
2020-08-04 07:59:28 -0700

    syntax bug fixed

--- backend/models/Style.model.js
+++ backend/models/Style.model.js
@@ -1,22 +1,25 @@
 const { Schema, model } = require("mongoose");
 
 const styleSchema = new Schema({
-  styles: {type: Object, default :{
-    header: {
-      background: "",
-      fontSize: "",
-    },
+  styles: {
+    type: Object,
+    default: {
+      header: {
+        background: "",
+        fontSize: "",
+      },
 
-    body: {
-      background: "white",
-      color: "",
-      fontSize: "",
+      body: {
+        background: "white",
+        color: "",
+        fontSize: "",
+      },
+      section: {
+        background: "",
+        color: "",
+        fontSize: "",
+      },
     },
-    section: {
-      background: "",
-      color: "",
-      fontSize: "",
-    }
   },
   userID: Schema.Types.ObjectId,
 });
